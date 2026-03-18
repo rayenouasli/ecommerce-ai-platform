5c49b87045c06990c6a290b697af090a2fbe2515
Rayen Wasli <135820926+rayenouasli@users.noreply.github.com>
2026-03-18 02:38:11 +0100

    Implementing queries and types

--- app/(app)/layout.tsx
+++ app/(app)/layout.tsx
@@ -1,10 +1,12 @@
 
+import { SanityLive } from "@/sanity/lib/live";
 import { ClerkProvider } from "@clerk/nextjs";
 
 function AppLayout({ children }: { children: React.ReactNode }) {
   return (
     <ClerkProvider>
             <main>{children}</main>
+            <SanityLive/>
     </ClerkProvider>
   );
 }
